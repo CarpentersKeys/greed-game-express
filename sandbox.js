8b5dc9c916582e46385eec8c2d5c0ab825522d4d
joshua <carpenterskeys@gmail.com>
2022-03-21 14:28:47 -0400

    built patientReduce to handle array of mixed promise and non promises

--- sandbox.js
+++
@@ -1,7 +0,0 @@
-// patientReduce auto curries the 
-
-patientReduce = function(i = 0) {
-
-    return console.log(this[i] + 3)
-
-}
--- util.js
+++ util.js
@@ -1,4 +1,5 @@
-export { deepClone, flattenToObj, patientArrayPolyfill }
+export { deepClone, flattenToObj, patientReduce}
+
 
 function deepClone(obj) {
     return JSON.parse(JSON.stringify(obj));
@@ -13,10 +14,27 @@ function flattenToObj(previousResults, ...newProps) {
     // the idea is to pass arguments[0] (results object param) with any new additions
 }
 
-function patientArrayPolyfill() {
-    if (Array.prototype.patientReducer) { return }
+function patientReduce(array, callback, initialValue) {
 
-    Array.prototype.patientArray = function () {
+    function reduceRecurcsively(arr, cb, prev, i) {
+        if (i < 0) throw new Error('index has been tampered with')
 
+        if (i >= arr.length) return prev
+        if (arr[i].then) {
+            // if there's a promise in the array wait for it
+            return arr[i]
+                .then(result => {
+                    const next = (cb(prev, result, i, arr));
+                    reduceRecurcsively(arr, cb, next, i + 1);
+                });
+
+        } else {
+            // if no promise, handle as usual
+            const next = cb(prev, array[i], i, arr)
+            return reduceRecurcsively(arr, cb, next, i + 1)
+
+        }
     }
+
+    return reduceRecurcsively(array, callback, initialValue, 0)
 }
