25ef4444b83594f65fccc4f97cb562408c2caecd
joshua <carpenterskeys@gmail.com>
2022-04-04 13:16:38 -0400

    currying, refactoring, doucmenting member access utilities and recursivelyRepalce fn

--- sandbox.js
+++ sandbox.js
@@ -82,34 +82,54 @@ import { emptyObjectsAndFlatten, filterForThenables, isObject } from "./util"
 function hasChildren(sth) {
     return Array.isArray(sth) || isObject(sth);
 }
+{// testing block
 
-// const data = {
-//     tim: 3,
-//     bill: 0,
-//     bie: [1]
-// }
+    // const data = {
+    //     tim: 3,
+    //     bill: 0,
+    //     bie: [1]
+    // }
 
-const data = [
-    { one: 2, },
-    {
-        three: [4, 5],
-        six: 7,
-    }
-]
+    const data = [
+        { one: 2, },
+        { one: 2, },
+        {
+            three: [4, 5],
+            six: 7,
+        }
+    ]
 
-const result = recursiveReplace(data, (e) => {
-    return e * 10
-})
+    const result = recursiveReplace(data, (e) => {
+        return e * 10;
+    });
 
-result
+    result
 
-// NEXT
+}
 function recursiveReplace(dataStructure, datumOperation) {
 
-    Object.freeze(dataStructure);
+    Object.freeze(dataStructure); // does this freeze it outside?
     // new data structure with the same shape as dataStructure
     const newStrct = JSON.parse(JSON.stringify(dataStructure));
     const previousMemberAccessStrings = new Set()
+
+    /** details
+     * @curryingFunction -> @conditionalFunction
+     * checking if two items are equal 
+     * and that a third isn't found on @previousMemberAccessStrings
+     * @param {*} target value to test
+     * @param {array} @previousMemberAccessStrings if, when calling the curried function, 
+     *                                             targets' accessString is found on this, return false
+     * @returns {function} that returns true if both conditions pass, otherwise, false
+     */
+    const curryEquals = (target) => {
+        // curry in the target
+        return function equalsNovel(isLike, memberAccessString) {
+            console.log(previousMemberAccessStrings);
+            if (previousMemberAccessStrings.has(memberAccessString)) { return false; };
+            return target === isLike;
+        }
+    };
 
     return (function recur(data = dataStructure) {
         if (data === undefined) { return; };
@@ -119,17 +139,18 @@ function recursiveReplace(dataStructure, datumOperation) {
             // at the position data is found on dataStructure
             // set newStrct to the return value of the callback of data
 
-            // the conditional function for finding the same member access string on the original dataStructure
-            const equalsData = e => { return e === data; };
+            // curry in the data to test
+            const equalsCondition = curryEquals(data);
             // get a memberAccessString from dataStrucutre that equals the current data and hasn't been used yet
-            const memberAccessString = computeMemberAccessString(dataStructure, equalsData, previousMemberAccessStrings)
+            const memberAccessString = computeMemberAccessString(dataStructure, equalsCondition)
             // if we found a working access string, add it to the Set of previously used ones
             if (memberAccessString) {
                 previousMemberAccessStrings.add(memberAccessString);
                 // perform operation on the data
                 const newDatum = datumOperation(data);
                 // assign the new data to the new data structure
-                newStrct[memberAccessString] = newDatum;
+                let member = accessByString(newStrct, memberAccessString);
+                member = newDatum;
             };
             return;
         }
@@ -150,89 +171,94 @@ function recursiveReplace(dataStructure, datumOperation) {
     }());
 }
 
-function accessComputedMember(dataStructure, accessString) {
-    return accessString
+/** details
+ * @function accessByString uses a @memberAccessString to return an element on a @dataStructure
+ * found solution https://stackoverflow.com/a/45322101/17551270
+ * @param {*} dataStructure complex object, can be arrays or objects nested deeply
+ * @param {string} memberAccessString '.' separated segments used to iteratively traverse the @dataStructure
+ * @param {bool -optional-} logErrors set to false to suppress
+ * @returns the accessed member of the dataStructure, or undefined if it doesn't exist (log error)
+ */
+function accessByString(dataStructure, memberAccessString, logErrors = true) {
+    if (typeof (memberAccessString) === 'number') { memberAccessString = memberAccessString + ''; };
+    if (typeof (memberAccessString) !== 'string') { return logErrors && console.error('accessByString: memberAccess not coercable to a string'); };
+    return memberAccessString
         .split('.')
         .reduce((prev, curr) => {
-            return prev[curr]
-        }, dataStructure)
+            return prev?.[curr]
+        }, dataStructure) || logErrors && console.error('accessByString: member not found on dataStructure')
 }
 
-// const cMASArr = [1, 2, 3, 4,
-//     [5,
-//         [7]
-//     ]
+// testing block
+// const cA = [1, 2, 3, 4,
+//     {
+//         five: 5,
+//         arr: [7]
+//     }
 // ]
 
-// const cMASResult = computeMemberAccessString(cMASArr, (e) => {
-//     e
-//     return e === 7;
+// const cO = {
+//     one: 1,
+//     two: 2,
+//     three: [
+//         4, 5
+//     ],
+// }
+
+
+// const cMASResult = computeMemberAccessString(cO, (e) => {
+//     return e === 4;
 // }
 // )
 
-// cMASResult
-// accessComputedMember(cMASArr, cMASResult)
+// cMASResult;
+// accessByString(cO, 'three.1')//?
 
-
-/**
- * Traverse a complex object and return a member access string for the first element that meets a given condition
+/** details
+ * Traverse a complex object and return an array of access segments for the first element that meets a given condition
  * Traversal is depth recursive and iterative across each layer
  * Traversal is breadth first, so first element found that meets the condition will be on highest order object
  * 
  * @param {*} dataStructure the object to traverse, can be an array or object, either can be nested deeply
  * @param {function} condition FUTURE: make optional, default equivalence fn
  *      @param {data} data tested
-*       @returns {boolean} result of conditional test on data given
- * @param {array -optional-} previousVals will be checked to prevent multiple returns of the same string
-                            // FUTURE: refactor previousVals to condition
- * @returns {string} of '.' separate member access segments (dot notation)
+ *      @param {string} accessString condition may need to check if it's been used before
+ *      @returns {boolean} result of conditional test on data given
+ * @param {bool -optional-} logErrors set to false to suppress
+ * @returns {array} of member access segments
  */
 
-function computeMemberAccessString(dataStructure, condition, previousVals) {
+function computeMemberAccessString(dataStructure, conditionFn, logErrors = true) { //? ${dataStructure} */
 
-    // convert array to a string on bracket notation
-    const joinAccessSegments = (arr) => {
-        if (!Array.isArray(arr) || arr === '' || arr.length < 2 || arr === []) { return arr }; // FUTURE clean this
-        return arr.map(e => {
-            return '[' + e + ']';
-        })
-            .join('')
-    }
-
-    // check data against condition and make sure value hasn't been used previoiusly 
-    // FUTURE: refactor previousVals to condition
-    const checkCondtion = (data, condition, accessString) => {
-        joinAccessSegments(accessString);
-        return !previousVals.has(accessString + '') && condition(data);
-    }
+    function checkCondition(data, accessSegmentsArray) {
+        const accessString = accessSegmentsArray.join('.');
+        return conditionFn(data, accessString);
+    };
 
 
-    const memberAccessString =
+    const memberAccessSegmentsArray =
 
-        (function recur(data, cond, accessString = []) { // recursive IIFE
+        /*main fn - recursive IIFE*/
+        (function recur(data = dataStructure, segArr = []) {
 
-            // if the condition of data is met and that access string hasn't been included in previousVals
-            const conditionMet = checkCondtion(data, cond, accessString);
-            // return that accessString
-            if (conditionMet) { return Object.freeze(accessString); };
+            // return that arr of member access segments
+            if (checkCondition(data, segArr)) { return segArr; };
 
 
             if (Array.isArray(data)) {
                 // try to satisfy the condition on this level
                 const ind = data.findIndex((e, i) => {
-                    return checkCondtion(e,
-                        cond,
-                        accessString.concat(i + ''));
+                    return checkCondition(e, segArr.concat(i + ''));
                 });
                 if (ind !== -1) {
-                    return Object.freeze(accessString.concat(ind + ''));
+                    return Object.freeze(segArr.concat(ind + ''));
                 };
 
                 // if not, go deeper
                 for (const datInd in data) {
                     const datum = data[datInd]
                     if (hasChildren(datum)) {
-                        return recur(datum, cond, accessString.concat(datInd + ''));
+                        return recur(datum, segArr.concat(datInd + ''));
                     };
                 };
             };
@@ -246,28 +272,37 @@ function computeMemberAccessString(dataStructure, condition, previousVals) {
                         .findIndex(e => {
                             const k = e[0] // key
                             const v = e[1] // value
-                            return checkCondtion(v, cond, accessString.concat(k + ''));
+                            return checkCondition(v, segArr.concat(k + ''));
                         });
                 const key = entries?.[ind]?.[0];
                 if (key) {
-                    return Object.freeze(accessString.concat(key + ''));
+                    return Object.freeze(segArr.concat(key + ''));
                 };
 
                 //if not, go deeper
                 for (const datProp in data) {
                     const datum = data[datProp]
                     if (hasChildren(datum)) {
-                        return recur(datum, cond, accessString.concat(datProp + ''));
+                        return recur(datum, segArr.concat(datProp + ''));
                     };
                 };
             };
-            return;
+            // if nothing was found
+            return undefined;
 
-        }(dataStructure, condition));
+        }());
 
-    if (memberAccessString) { return joinAccessSegments(memberAccessString) };
-    return console.log(`condition: [${condition.name}] not met on dataStructure: [${dataStructure}]`);
+    /**
+     * @returns @param {string -imut-} memberAccessString of '.' separated access segments
+     * @example accessByString(dataStructure, memberAccessString = 'default.access.4.0')
+     */
+    if (memberAccessSegmentsArray) {
+        return Object.freeze(memberAccessSegmentsArray.join('.'));
+    };
+    // nothing found that met the condition
+    if (logErrors) {
+        return console.error(`computeMemberAccessString: \ncondition: [${conditionFn.name}] not met on dataStructure: [${dataStructure}]`);
+    }
 };
 
-// rename things called string that are arrays
-// FUTURE: refactor previousVals to condition
+// NEXT: fix bug causing ceckCondition to fail
